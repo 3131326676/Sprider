f2679d0347a5a2d687314d4e59edcbd1a606e997
3131326676 <3131326676@qq.com>
2019-06-24 11:10:23 +0800

    豆瓣首页图书信息的爬取

--- douban.py
+++ douban.py
@@ -6,7 +6,7 @@ pattern = re.compile(
     '<li.*?cover.*?href="(.*?)".*?title="(.*?)".*?more-meta.*?author">(.*?)</span>.*?year">(.*?)</span>.*?</li>',
     re.S)
 results = re.findall(pattern, content)
-# print(results)
+print(results)
 for result in results:
     url, name, author, date = result
     author = re.sub('\s', '', author)
